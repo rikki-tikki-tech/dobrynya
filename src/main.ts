73854f23bcdf7f375243febfb642498504f9ceba
Egor Utrobin <eg.utrobin@yandex.ru>
2024-07-15 19:08:49 +0200

    CORS. Allow credentials

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3001/'],
-    credentials: true,
-    allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'X-Requested-With',
-      'Accept',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  });
   app.use(cookieParser());
+  app.use(cors());
 
   await app.listen(3000);
 }
