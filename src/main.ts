300a869190f1ed524bf4a15c48351c53583b2e8c
Egor Utrobin <eg.utrobin@yandex.ru>
2024-07-15 13:18:54 +0200

    CORS. Allow all host

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
   app.use(cookieParser());
 
   await app.listen(3000);
